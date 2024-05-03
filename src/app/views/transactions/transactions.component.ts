4e5ad51c283e3f5c5b031a6de63bc4295f104d51
jayrboy <j.jayox_@hotmail.com>
2024-05-03 16:35:36 +0700

    add sorting transaction

--- src/app/views/transactions/transactions.component.ts
+++ src/app/views/transactions/transactions.component.ts
@@ -4,12 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { TransactionService } from '../../services/transaction.service';
 import Transaction from '../../models/transaction.model';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
   templateUrl: './transactions.component.html',
-  imports: [RouterModule, CommonModule, FormsModule],
+  imports: [RouterModule, CommonModule, FormsModule, MatIconModule],
 })
 export class TransactionsComponent implements OnInit {
   AllTransaction: Transaction[] = [];
@@ -39,10 +40,10 @@ export class TransactionsComponent implements OnInit {
   onKey(event: any) {
     this.searchProductName = event.target?.value;
 
-    console.log({
-      name: this.searchProductName,
-      transactionTypeId: this.idTransactionType,
-    });
+    // console.log({
+    //   name: this.searchProductName,
+    //   transactionTypeId: this.idTransactionType,
+    // });
 
     this.showTransaction = this.AllTransaction.filter((t) => {
       return (
@@ -50,7 +51,74 @@ export class TransactionsComponent implements OnInit {
         t.transactionTypeId == parseInt(this.idTransactionType)
       );
     });
+    // console.log(this.showTransaction);
+  }
 
-    console.log(this.showTransaction);
+  isAscendingOrder: boolean = true;
+
+  onSortId() {
+    if (this.isAscendingOrder) {
+      this.showTransaction = this.showTransaction.sort((a, b) => b.id - a.id); // จากมากไปน้อย
+    } else {
+      this.showTransaction = this.showTransaction.sort((a, b) => a.id - b.id); // จากน้อยไปมาก
+    }
+
+    this.isAscendingOrder = !this.isAscendingOrder;
+  }
+
+  onSortProduct() {
+    if (this.isAscendingOrder) {
+      this.showTransaction = this.showTransaction.sort(
+        (a, b) => b.productId - a.productId
+      ); // จากมากไปน้อย
+    } else {
+      this.showTransaction = this.showTransaction.sort(
+        (a, b) => a.productId - b.productId
+      ); // จากน้อยไปมาก
+    }
+
+    this.isAscendingOrder = !this.isAscendingOrder;
+  }
+
+  onSortTransaction() {
+    if (this.isAscendingOrder) {
+      this.showTransaction = this.showTransaction.sort(
+        (a, b) => b.transactionTypeId - a.transactionTypeId
+      ); // จากมากไปน้อย
+    } else {
+      this.showTransaction = this.showTransaction.sort(
+        (a, b) => a.transactionTypeId - b.transactionTypeId
+      ); // จากน้อยไปมาก
+    }
+
+    this.isAscendingOrder = !this.isAscendingOrder;
+  }
+
+  onSortQuantity() {
+    if (this.isAscendingOrder) {
+      this.showTransaction = this.showTransaction.sort(
+        (a, b) => b.quantity - a.quantity
+      ); // จากมากไปน้อย
+    } else {
+      this.showTransaction = this.showTransaction.sort(
+        (a, b) => a.quantity - b.quantity
+      ); // จากน้อยไปมาก
+    }
+
+    this.isAscendingOrder = !this.isAscendingOrder;
+  }
+
+  onSortUpdateDate() {
+    if (this.isAscendingOrder) {
+      this.showTransaction = this.showTransaction.sort(
+        (a, b) => b.updateDate - a.updateDate
+      ); // จากมากไปน้อย
+    } else {
+      this.showTransaction = this.showTransaction.sort(
+        (a, b) => a.updateDate - b.updateDate
+      ); // จากน้อยไปมาก
+    }
+
+    this.isAscendingOrder = !this.isAscendingOrder;
   }
 }
